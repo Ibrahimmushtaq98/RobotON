595d7bbe07cc8da37fbe4bd65fdfff072019901c
Ibrahim Mushtaq <ibrahim.mushtaq@uoit.net>
2019-06-07 12:32:51 -0400

    Added Routes for the second Database and removed some files

--- RobotON DB/api/controllers/controller.js
+++ RobotON DB/api/controllers/controller.js
@@ -1,9 +1,12 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-    Task = mongoose.model('RobotON_Logs');
+    Task = mongoose.model('RobotON_Logs'),
+    TaskT = mongoose.model('RobotBug_Logs');
 
-exports.list_all_logs = function(req,res){
+
+//GET REQUEST FOR ALL DATA UNDER ON
+exports.list_all_logs_ON = function(req,res){
     Task.find({}, function(err, task) {
         if (err)
           res.send(err);
@@ -11,7 +14,7 @@ exports.list_all_logs = function(req,res){
       });
 };
 
-exports.create_a_log = function(req, res) {
+exports.create_a_log_ON = function(req, res) {
   console.log(req.body);
   var new_task = new Task(req.body);
   new_task.save(function(err, task) {
@@ -23,7 +26,7 @@ exports.create_a_log = function(req, res) {
 };
 
 
-exports.read_a_log = function(req, res) {
+exports.read_a_log_ON = function(req, res) {
     console.log(req.params);
   Task.findOne({name: req.params.sessionID}, function(err, task) {
     if (err)
@@ -33,7 +36,7 @@ exports.read_a_log = function(req, res) {
 };
 
 
-exports.update_a_log = function(req, res) {
+exports.update_a_log_ON = function(req, res) {
   console.log(req.body);
   Task.findOneAndUpdate({name: req.params.sessionID}, 
     {$push : {levels: req.body['levels']}}, 
@@ -51,12 +54,51 @@ exports.update_a_log = function(req, res) {
   Task.find
 };
 
+//------------------------------------------------------------------------------->
+exports.list_all_logs_BUG = function(req,res){
+  TaskT.find({}, function(err, task) {
+      if (err)
+        res.send(err);
+      res.json(task);
+    });
+};
 
-// exports.update_a_log = function(req, res) {
-//     Task.findOneAndUpdate({name: req.params.sessionID}, req.body.levels, {new: true}, function(err, task) {
-//       if (err)
-//         res.send(err);
-//       res.json(task);
-//     });
-//   };
+exports.create_a_log_BUG = function(req, res) {
+  var new_task = new TaskT(req.body);
+  new_task.save(function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+    console.log(task);
+  });
+};
+
+
+exports.read_a_log_BUG = function(req, res) {
+  console.log(req.params);
+  TaskT.findOne({name: req.params.sessionID}, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+
+exports.update_a_log_BUG = function(req, res) {
+  console.log(req.body);
+  TaskT.findOneAndUpdate({name: req.params.sessionID}, 
+    {$push : {levels: req.body['levels']}}, 
+    {new: true},
+    function(err, task) {
+    console.log("PUT");
+    console.log(req.body);
+    if (err)
+      res.send(err);
+    
+    if(task == null)
+    res.json(task);
+    console.log(task);
+  });
+  TaskT.find
+};
 
--- RobotON DB/api/routes/routes.js
+++ RobotON DB/api/routes/routes.js
@@ -4,11 +4,19 @@ module.exports = function(app){
     var logControl = require('../controllers/controller');
 
     app.route('/logsON')
-    .get(logControl.list_all_logs)
-    .post(logControl.create_a_log);
+    .get(logControl.list_all_logs_ON)
+    .post(logControl.create_a_log_ON);
 
     app.route('/logsON/:sessionID')
-    .get(logControl.read_a_log)
-    .put(logControl.update_a_log)
+    .get(logControl.read_a_log_ON)
+    .put(logControl.update_a_log_ON);
+
+    app.route('/logsBUG')
+    .get(logControl.list_all_logs_BUG)
+    .post(logControl.create_a_log_BUG);
+
+    app.route('/logsBUG/:sessionID')
+    .get(logControl.read_a_log_BUG)
+    .put(logControl.update_a_log_BUG);
 
 };
