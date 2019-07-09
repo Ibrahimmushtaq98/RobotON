ae349a1b35801660c018fbaaeba0cbb0d4304f01
Ibrahimmushtaq98 <ibrahim.mushtaq@uoit.net>
2019-07-09 13:34:31 -0400

    forgot to add toString method to roboON

--- RobotON DB/api/controllers/controller.js
+++ RobotON DB/api/controllers/controller.js
@@ -73,14 +73,14 @@ exports.retrieve_comp_level_ON = function(req, res){
           currentLevel = value.name;
 
           if(value.progress == "Passed" && currentLevel != ""){
-            compLevel.push(currentLevel);
+            compLevel.push(currentLevel + " " + 1);
             currentLevel = "";
           }
         });
       };
       compLevel = compLevel.filter(onlyUnique);
       //sendBack = compLevel.sort();
-      res.json(compLevel);
+      res.json(compLevel.toString());
     }
   });
 }
