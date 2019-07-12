5169b6d6fd9380cdf00aed8855d5a4b7c189aed2
Ibrahim Mushtaq <ibrahim.mushtaq@uoit.net>
2019-07-12 13:21:34 -0400

    Fixed small issue regarding about path

--- RobotON Server/CompileProject.sh
+++ RobotON Server/CompileProject.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 git pull
 git reset --hard
+cd ../
 echo Now Compiling Project
 /home/ibrahim/Unity/Hub/Editor/2019.2.0b7/Editor/Unity -quit -force-free -nographics -batchmode -logFile stdout.log -projectPath /home/ibrahim/Desktop/RobotON/ -executeMethod WebGLBuilder.build
 echo Finished Job, check Logs!
+cd RobotON\ Server
 node mailingService.js
 cd ../
 xfce4-terminal --working-directory=/home/ibrahim/Desktop/RobotON/Bat-Term\ Script/ -e './CopyFIleToServerLinux.sh'
