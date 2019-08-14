a162f99a9443c49f51edeed94462a9f09fcee5d5
Ibrahim Mushtaq <ibrahim.mushtaq@uoit.net>
2019-08-14 11:41:18 -0400

    Fixed the WebGlBuilder Script

--- Assets/Editor/WebGLBuilder.cs
+++ Assets/Editor/WebGLBuilder.cs
@@ -1,23 +1,57 @@
-﻿using System.Collections;
+﻿using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build.Reporting;
 
 public class WebGLBuilder
 {
     static void build() {
+        File.AppendAllText(@"/home/ibrahim/Desktop/RobotON/stdout1.log", "Build Started!, time: " + System.DateTime.Now.ToString());
 
-    // Place all your scenes here
-    string[] scenes = {"Assets/TitleScene.unity", 
-                        "Assets/MainMenu.unity",
-                        "Assets/newgame.unity",
-                        "Assets/Cinematic.unity",
-                        "Assets/CharacterSelect.unity",
-                        "Assets/IntroScene.unity",
-                        "Assets/Transition.unity",
-                        "Assets/Credits.unity"};
 
-    string pathToDeploy = "builds/WebGLversion/";       
+        BuildPlayerOptions buildPlayer = new BuildPlayerOptions();
+        buildPlayer.scenes = new [] {"Assets/TitleScene.unity",
+                            "Assets/TitleMenu.unity",
+                            "Assets/StartScene.unity",
+                            "Assets/Leaderboard.unity", 
+                            "Assets/MainMenu.unity",
+                            "Assets/newgame.unity",
+                            "Assets/Cinematic.unity",
+                            "Assets/CharacterSelect.unity",
+                            "Assets/IntroScene.unity",
+                            "Assets/Transition.unity",
+                            "Assets/Credits.unity",
+                            "Assets/TutorialDemo.unity",
+                            "Assets/Progression.unity"};
 
-    BuildPipeline.BuildPlayer(scenes, pathToDeploy, BuildTarget.WebGL, BuildOptions.None);      
+        buildPlayer.locationPathName = "/home/ibrahim/Desktop/WebGLBuilds";
+        buildPlayer.target = BuildTarget.WebGL;
+        Application.targetFrameRate = 30;
+
+        PlayerSettings.WebGL.dataCaching = true;
+        PlayerSettings.defaultWebScreenHeight = 720;
+        PlayerSettings.defaultWebScreenWidth = 1280;
+        PlayerSettings.runInBackground = true;
+        AspectRatio aspectRatio = AspectRatio.Aspect16by9;
+        PlayerSettings.SetAspectRatio(aspectRatio, true);
+        PlayerSettings.productName = "RoboGames";
+        PlayerSettings.companyName = "UOIT";
+
+        WebGLExceptionSupport web = WebGLExceptionSupport.FullWithoutStacktrace;
+        PlayerSettings.WebGL.exceptionSupport = web;
+        Application.runInBackground = true;
+       // buildPlayer.options = (BuildOptions)web | (BuildOptions)compressionFormat;
+        buildPlayer.options = (BuildOptions)web;
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayer);
+        BuildSummary summary = report.summary;
+
+        if(summary.result == BuildResult.Succeeded){
+            File.AppendAllText(@"/home/ibrahim/Desktop/RobotON/stdout1.log", "Build Succed: " + summary.totalSize + " bytes, Date :" + System.DateTime.Now.ToString()+"\n");
+
+        }else if(summary.result == BuildResult.Failed){
+            File.AppendAllText(@"/home/ibrahim/Desktop/RobotON/stdout1.log", "Build Failed: " + summary.totalSize + " bytes, Date :" + System.DateTime.Now.ToString()+"\n");
+        }
     }
 }
--- RobotON Server/CompileProject.sh
+++ RobotON Server/CompileProject.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-git pull
 git reset --hard
+git pull
 cd ../
 echo Now Compiling Project
 /home/ibrahim/Unity/Hub/Editor/2019.2.0b7/Editor/Unity -quit -force-free -nographics -batchmode -logFile stdout.log -projectPath /home/ibrahim/Desktop/RobotON/ -executeMethod WebGLBuilder.build
