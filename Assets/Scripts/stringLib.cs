49e7092e1f94c207c25bbfa21de83ffb51b15042
Ibrahimmushtaq98 <ibrahim.mushtaq@ontariotechu.net>
2019-09-12 18:16:55 -0400

    Fixed server URL

--- Assets/Scripts/stringLib.cs
+++ Assets/Scripts/stringLib.cs
@@ -200,8 +200,8 @@ public const string XML_ATTRIBUTE_OPTIONS = "options";
     public string syntax_color_include = "<color=#e6005cff>";
     public float LEFT_CODESCREEN_X_COORDINATE = -10;
 
-    public const string SERVER_URL = "https://robogames.uoit.science.ca/";
-    public const string DB_URL = "https://robogames.uoit.science.ca/logs";
+    public const string SERVER_URL = "https://robogames.science.uoit.ca/";
+    public const string DB_URL = "https://robogames.science.uoit.ca//logs";
     //public const string DB_URL = "http://127.0.0.1:3000/logs";
     public const string CURRENT_LEVEL = "currentlevel/";
     public const int DOWNLOAD_TIME = 200;
